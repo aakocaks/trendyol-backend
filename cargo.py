a07ae0ee3e90d933b0e40ff3fcf74801e1ad758b
aakocaks <aakocaks@gmail.com>
2026-01-13 00:28:20 +0300

    Delete cargo.py

--- cargo.py
+++
@@ -1,9 +0,0 @@
-CARGO_PRICES = {
-    "Sürat Kargo": 65,
-    "Yurtiçi Kargo": 70,
-    "MNG Kargo": 75,
-    "Aras Kargo": 72
-}
-
-def calculate_cargo_cost(order):
-    return CARGO_PRICES.get(order.get("cargoProviderName"), 70)
