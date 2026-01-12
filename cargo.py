d882637179f7b50b7b0edaf56095f3de11b93b09
aakocaks <aakocaks@gmail.com>
2026-01-13 00:12:42 +0300

    Update cargo.py

--- cargo.py
+++ cargo.py
@@ -7,8 +7,3 @@ CARGO_PRICES = {
 
 def calculate_cargo_cost(order):
     return CARGO_PRICES.get(order.get("cargoProviderName"), 70)
-
-def calculate_cargo(price: float) -> float:
-    if price >= 300:
-        return 0.0
-    return 29.90
