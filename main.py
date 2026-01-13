e12e78e98aadbc5516e4b090a879b045c40a1841
aakocaks <aakocaks@gmail.com>
2026-01-13 11:05:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"ok": True}
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"ok": True}
+
+
+@app.get("/health")
+def health():
+    return {
+        "service": "trendyol-backend",
+        "status": "running"
+    }
